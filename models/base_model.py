a3176e3d2cd4315eb9d2ac921eca487428118483
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-03-06 00:56:50 +0100

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ initialize a BaseModel instance """
         self.id = uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if "created_at" == key:
@@ -25,4 +27,28 @@ class BaseModel:
                     self.updated_at = datetime.strptime("%Y-%m-%dT%H:%M:%S.%f", kwargs["updated_at"])
                 elif "__class__" == key:
                     pass
-                
+        
+        
+    def __str__(self):
+        """ String representation of BaseModel """
+        str_ = "[{}] ({}) {}"
+        str_ = str_.format(self.__class__.__name__, self.id, self.__dict__)
+        return str_
+        
+    def save(self):
+        """ public method that updates instance attribute
+            update_at with the current date
+         """
+        self.updated_at = datetime.now()
+        
+            
+    def to_dict(self):
+        """ Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict_ = self.__dict__.copy()
+        dict_["__class__"] = self.__class__.__name__
+        dict_["created_at"] = self.created_at.isoformat()
+        dict_["updated_at"] = self.updated_at.isoformat()
+        return dict_
+        
