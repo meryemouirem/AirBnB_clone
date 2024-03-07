b45a6afa0ba893d3f212c44fffe78630db4c8d79
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-03-07 21:24:45 +0100

    Command line interface: implemented  EOF and quit

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 """
 The Command line interface of the entire application
@@ -21,13 +23,28 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """
-        quit command to exit the shell
+        quit command to exit the program
         """
         return True
 
     def emptyline(self) -> bool:
         """ Do nothing upon a new lie """
         pass
+
+    def do_create(self, arg):
+        """ creates new instance of a BaseModel, and save it into
+         a JSON file
+         """
+        try:
+            obj = eval(arg)()
+            obj.save()
+        except SyntaxError:
+            print("** class missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        else:
+            print(obj.id)
+
 
 
 if __name__ == "__main__":
