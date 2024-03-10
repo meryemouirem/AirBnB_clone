5f9b2fa11d909b8267faa4b961d9ee87ab72e6cd
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-03-10 15:28:21 +0000

    correction of unit tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -14,7 +14,7 @@ class TestAmenity(unittest.TestCase):
         self.new_amenity = Amenity()
 
     def test_attributes_existence(self):
-        """existince"""
+        """ tests attributes existence """
         attributes = ["id", "created_at", "updated_at", "name"]
         for attr in attributes:
             with self.subTest(attr=attr):
