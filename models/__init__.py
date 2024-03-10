b4cb9d35e5a0f864aaba2d4c2d82e286502eafb6
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-03-10 14:50:14 +0000

    pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
+"""Initialize a FileStorage instance"""
 from models.engine.file_storage import FileStorage
-"""
-Initialize a FileStorage instance
-"""
 storage = FileStorage()
 storage.reload()
